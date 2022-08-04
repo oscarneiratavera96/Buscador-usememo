da84990b406fa19f72935d7ef3f8dcbc94e7d77c
oscaryesidneira <oscarneira@unisangil.edu.co>
2022-08-04 00:41:06 -0500

    Buscador usando Usememo-v1.0

---
+++ src/components/marketItem.js
@@ -0,0 +1,55 @@
+import { useMemo } from "react";
+import styled from "styled-components";
+
+const StyledMarker = styled.span`
+  background-color: yellow;
+  font-weight: bolder;
+  border-radius: 2px;
+`;
+
+const StyledItem = styled.a`
+  color: black;
+  display: block;
+  padding: 10px;
+  text-decoration: none;
+  &:hover {
+    background-color: #4c91ba;
+    color: white;
+  }
+  &:hover span {
+    color: black;
+  }
+`;
+
+const MarketItem = ({ item, query, onClick }) => {
+  const { left, center, right } = useMemo(
+    () => getPositions(item, query),
+    [item, query]
+  );
+
+  function getPositions(item, query) {
+    const index = item.title.toLowerCase().indexOf(query);
+    const left = item.title.slice(0, index);
+    const right = item.title.slice(index + query.length);
+    const center = item.title.slice(index, index + query.length);
+
+    return {
+      left,
+      center,
+      right,
+    };
+  }
+
+  function handleClick() {
+    onClick(item);
+  }
+  return (
+    <StyledItem href="#" onClick={handleClick}>
+      {left}
+      <StyledMarker> {center}</StyledMarker>
+      {right}
+    </StyledItem>
+  );
+};
+
+export default MarketItem;
---
+++ src/components/results.js
@@ -0,0 +1,49 @@
+import { useEffect, useMemo, useState } from "react";
+import MarketItem from "./marketItem";
+import styled from "styled-components";
+
+const ResultsContainer = styled.div`
+  position: absolute;
+  width: 400px;
+  background: white;
+  border: solid 1px #222;
+  border-top: solid 1px transparent;
+  margin-top: -3px;
+  box-sizing: border-box;
+  border-radius: 0 0 5px 5px;
+`;
+
+const Results = ({ items, onItemSelected, query, onResultsCalculated }) => {
+  const [results, setResults] = useState([]);
+  const filteredItems = useMemo(() => findMatch(items, query), [items, query]);
+
+  useEffect(() => {
+    onResultsCalculated(results);
+  }, [results]);
+
+  function findMatch() {
+    const res = items.filter((i) => {
+      return i.title.toLowerCase().indexOf(query) >= 0 && query.length > 0;
+    });
+
+    setResults(res);
+    return res;
+  }
+
+  return (
+    <ResultsContainer>
+      {query !== ""
+        ? filteredItems.map((item) => (
+            <MarketItem
+              key={item.id}
+              item={item}
+              query={query}
+              onClick={onItemSelected}
+            />
+          ))
+        : ""}
+    </ResultsContainer>
+  );
+};
+
+export default Results;
